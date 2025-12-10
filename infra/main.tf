4aee74ce31f25fdca219013a1dbb2c08f6e83030
Festiveokagbare <festiveokotieboh@gmail.com>
2025-12-10 14:08:37 +0100

    added configmap secrets

--- infra/main.tf
+++ infra/main.tf
@@ -22,7 +22,7 @@ module "eks" {
   source          = "./modules/eks"
 
   cluster_name    = "bg-eks"
-  cluster_version = "1.28"
+  cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
